5fc4300c4c4f22c424d6a9f63e1b74f7f6acdd95
Mateusz <kontakt@mateuszdomagala.pl>
2021-07-22 19:12:19 +0200

    feat: add Font Awesome icons

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,5 +1,8 @@
 <template>
-    <input v-model="keyword" @keyup="searchCountry" type="search" placeholder="Search for a country...">
+    <div class="search">
+        <font-awesome-icon class="search__icon" icon="search" />
+        <input class="search__input" v-model="keyword" @keyup="searchCountry" type="search" placeholder="Search for a country...">
+    </div>
 </template>
 
 <script>
@@ -19,14 +22,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  input {
+  .search {
     background-color: var(--box-color);
-    border: 0;
     box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 4px;
     padding: 15px 25px;
     border-radius: 5px;
     color: var(--font-color);
     margin-bottom: 30px;
+
+    &__icon {
+        color: inherit;
+        margin-right: 15px;
+    }
+
+    &__input {
+        border: 0;
+        background-color: inherit;
+        color: inherit;
+    }
 
     @media (min-width: 660px) {
         width: 400px;
--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSearch)
+
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount("#app");
